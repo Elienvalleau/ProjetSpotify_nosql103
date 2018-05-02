b45286725a55c0cc7921327d5baa9e73ba611f55
elien <elienvalleau@gmail.com>
2018-05-02 12:20:30 +0200

    UPDATE : modeles mongoose

--- models/musics.js
+++ models/musics.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const Musics = db.model('Music', {
+  musicId: { type: String, required: true },
   musicName: { type: String, required: true },
   date:     { type: Date, required: true, default: Date.now }
 });
--- models/rooms.js
+++ models/rooms.js
@@ -1,5 +1,6 @@
 const db = require('../db');
 const Rooms = db.model('Room', {
+  roomId: { type: String, required: true },
   roomNname: { type: String, required: true },
   date:     { type: Date, required: true, default: Date.now }
 });
