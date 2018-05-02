05c8cb0bdc5b70641796718d9a54f014e4f8f8c6
elien <elienvalleau@gmail.com>
2018-05-02 12:41:12 +0200

    UPDATE : connexion bdd depuis index

--- db.js
+++ db.js
@@ -4,34 +4,8 @@ mongoose.connect('mongodb://localhost/spotyroom', function () {
 });
 module.exports = mongoose;
 
-
-
-// const MongoClient = require('mongodb').MongoClient;
-//
-// var state = {
-//   db: null,
-// };
-//
-// exports.connect = function(url, done) {
-//   if (state.db) return done();
-//
-//   MongoClient.connect(url, function(err, db) {
-//     if (err) return done(err);
-//     state.db = db;
-//     done()
-//   })
-// };
-//
-// exports.get = function() {
-//   return state.db
-// };
-//
-// exports.close = function(done) {
-//   if (state.db) {
-//     state.db.close(function(err, result) {
-//       state.db = null;
-//       state.mode = null;
-//       done(err)
-//     })
-//   }
-// };
+exports.connect = function() {
+  mongoose.connect('mongodb://localhost/spotyroom', function () {
+    console.log('mongodb connected')
+  });
+};
--- models/users.js
+++ models/users.js
@@ -4,22 +4,3 @@ const Users = db.model('User', {
   date:     { type: Date, required: true, default: Date.now }
 });
 module.exports = Users;
-
-
-// const db = require('../db');
-//
-// exports.insertTest = function() {
-//   // const collection = db.get().collection('users');
-//   let collection = db.get().collection('users');
-//
-//   collection.insert([{name: 'testSpot1'}, {name: 'testSpot2'}]);
-//
-// };
-//
-// exports.all = function(cb) {
-//   const collection = db.get().collection('users');
-//
-//   collection.find().toArray(function(err, docs) {
-//     cb(err, docs)
-//   })
-// };
