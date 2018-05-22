cb1b7b235d81fb8cfd3fe5ca2a04b54deb6848aa
Thomas Baudou <thomas.baudou33@hotmail.com>
2018-05-22 09:28:06 +0200

    First room controller

--- models/rooms.js
+++ models/rooms.js
@@ -1,7 +1,8 @@
 const db = require('../db');
 const Rooms = db.model('Room', {
   roomId: { type: String, required: true },
-  roomNname: { type: String, required: true },
+  roomName: { type: String, required: true },
   date:     { type: Date, required: true, default: Date.now }
 });
+
 module.exports = Rooms;
