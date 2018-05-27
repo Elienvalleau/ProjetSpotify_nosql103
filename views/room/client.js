ef1e6a11a8aab188889b1fcc604dd55169ef58d4
Jules Persch <jules.persch@ynov.com>
2018-05-27 18:02:00 +0200

    Search bar music
    Rajout d'une barre de recherche de musique, lance la première musique trouvé

--- views/room/client.js
+++ views/room/client.js
@@ -36,3 +36,13 @@ playPause.onclick = function() {
 next.onclick = function() {
   socket.emit('next')
 };
+
+$('#right form').submit(function (e) {
+  e.preventDefault();
+  const search = {text : $('#searchMusic').val()};
+  $('#searchMusic').val('');
+  if (search.text.trim().length !== 0) {
+    socket.emit('searchMusic', search);
+  }
+  $('#right input').focus();
+});
